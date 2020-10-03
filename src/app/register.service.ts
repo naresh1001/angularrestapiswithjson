843bba7a6405467a0610acb490d84b08cc4a712b
Naresh <naresh.mmmec@gmail.com>
2020-10-03 13:53:13 +0530

    first commit

--- src/app/register.service.ts
+++ src/app/register.service.ts
@@ -10,5 +10,14 @@ export class RegisterService {
 
   createUser(user){
      return this._http.post("http://localhost:3000/users",user);
+ }
+ getUsers(){
+   return this._http.get("http://localhost:3000/users");
  } 
+ deleteUser(user){
+  return this._http.delete("http://localhost:3000/users/"+user.id);
+ }
+ updateUser(user){
+  return this._http.put("http://localhost:3000/users/" +user.id,user)
+}
 }
--- src/app/registeruser/registeruser.component.ts
+++ src/app/registeruser/registeruser.component.ts
@@ -8,16 +8,50 @@ import { RegisterService } from '../register.service';
   styleUrls: ['./registeruser.component.css']
 })
 export class RegisteruserComponent implements OnInit {
+  allUsers: Object;
+  isEdit =false;
+  userObj={
+    username:'',
+    email:'',
+    password:'',
+    address:'',
+    mobile:'',
+    id:''
+  }
 
   constructor(private registerService:RegisterService) { }
 
   ngOnInit() {
+    this.getAllUsers();
   }
   doRegister(user){
     //console.log(user);
     this.registerService.createUser(user).subscribe((response)=>{
       console.log("value Inserted");
+      this.getAllUsers(); 
     })
+  }
+  getAllUsers(){
+   this.registerService.getUsers().subscribe((response)=>{
+     this.allUsers=response;
+   }) 
+  }
+  deleteMe(user){
+    this.registerService.deleteUser(user).subscribe((response)=>{
+    this.getAllUsers(); 
+    })
+  }
+  editUser(user){
+    this.isEdit=true;
+    this.userObj=user;
+  }  
+  updateUser(userForm){
+    this.isEdit =!this.isEdit;
+    this.registerService.updateUser(this.userObj).subscribe((response)=>{
+      this.getAllUsers();
+      userForm.reset();
+  })
+  
   }
 
 }
